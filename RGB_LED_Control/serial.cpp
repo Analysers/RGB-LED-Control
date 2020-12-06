cce404a22821f9c8f66f128d9d7f4f8351079093
developer <developer.01@ensatecnologia.com.br>
2020-12-06 17:15:44 -0300

    Finished project

---
+++ RGB_LED_Control/serial.cpp
@@ -0,0 +1,40 @@
+// ********************************************************************************************
+//                                     RGB_LED_Control                                                      
+// --------------------------------------------------------------------------------------------
+//
+// File  : serial.cpp
+// Author: Gabriel da Silva Caetano <98gabrielSc@gmail.com>
+//
+// ********************************************************************************************
+
+// ----------------------------------------------------------------------------------------- //
+
+#include "Headers/serial.h"
+
+// ----------------------------------------------------------------------------------------- //
+
+// ########### ********************************************************************************
+//
+// Function   : initializeSerial
+//
+// Parameters : void
+// Return     : void
+//
+// Description: Initializes device serial communication
+//
+// ########### ********************************************************************************
+
+void initializeSerial(void ) {
+
+    #if defined(SERIAL_ENABLED)
+
+        Serial.begin(BAUD_RATE_SERIAL); while(!Serial); Serial.println(F("."));
+
+    #endif
+
+    #if defined(SERIAL_1_ENABLED)
+    
+        Serial1.begin(BAUD_RATE_SERIAL_1); while(!Serial1); Serial1.println(F("."));
+
+    #endif
+}
